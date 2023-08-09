4406cfb6565ddd58c6a385eee1085b441ec2e955
lunavamp <104728015+lunavamp@users.noreply.github.com>
2023-08-09 19:28:54 +0200

    Update WeatherForecast.js

--- src/components/weather_forecast/WeatherForecast.js
+++ src/components/weather_forecast/WeatherForecast.js
@@ -6,7 +6,7 @@ const WeatherForecast = ({ selectedTrip }) => {
 
   useEffect(() => {
     if (!selectedTrip) return;
-    const API_KEY = "BG4XKLVSMAVY6BPX2XCUG7H7G";
+    const API_KEY = "";
     const apiUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${selectedTrip.city}/${selectedTrip.startDate}/${selectedTrip.endDate}?unitGroup=metric&include=days&key=${API_KEY}&contentType=json`;
 
     fetch(apiUrl)
